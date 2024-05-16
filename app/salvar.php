c070dfd606947c3aecf38c5c176f7bee98b0d5cd
mestredainfo <>
2024-05-16 20:57:43 -0300

    Add a licença e o Content-Security-Policy

--- app/salvar.php
+++ app/salvar.php
@@ -1,3 +1,12 @@
+<?php
+// Copyright (C) 2004-2024 Murilo Gomes Julio
+// SPDX-License-Identifier: GPL-2.0-only
+
+// Organização: Mestre da Info
+// Site: https://linktr.ee/mestreinfo
+
+header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline'");
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 
