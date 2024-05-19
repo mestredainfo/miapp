5d781b25f5b34de813490eb85ed6bcb9e955c848
mestredainfo <>
2024-05-18 21:59:09 -0300

    Adicionado um arquivo de exemplo.

---
+++ app/mensagem.php
@@ -0,0 +1,43 @@
+<?php
+// Copyright (C) 2004-2024 Murilo Gomes Julio
+// SPDX-License-Identifier: GPL-2.0-only
+
+// Organização: Mestre da Info
+// Site: https://linktr.ee/mestreinfo
+
+header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline'");
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mensagem</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <button type="button" onclick="mensagem()">Exibir Mensagem</button>
+    <button type="button" onclick="confirm()">Confirmar Mensagem</button>
+    <div id="info"></div>
+    <script>
+        async function mensagem() {
+            window.miapp.message('Mensagem de Informação', 'Este é um exemplo de mensagem!', 'info');
+        }
+
+        async function confirm() {
+            window.miapp.confirm('Mensagem de Confirmação', 'Este é um exemplo de mensagem!', 'error').then((result) => {
+                console.log(result);
+                if (result) {
+                    document.getElementById('info').innerHTML = 'Não Confirmado';
+                } else {
+                    document.getElementById('info').innerHTML = 'Confirmado';
+                }
+            });
+        }
+    </script>
+</body>
+
+</html>
