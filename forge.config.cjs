eb84573b15c30e6b6b63e761ae7eb65a465cb420
mestredainfo <>
2024-05-24 20:53:35 -0300

    Incluido a plataforma win32 e o hooks para após a compilação

--- forge.config.cjs
+++ forge.config.cjs
@@ -10,9 +10,25 @@ module.exports = {
     {
       name: "@electron-forge/maker-zip",
       platforms: [
+        'win32',
         "linux"
       ]
     }
   ],
+  hooks: {
+    postPackage: async (config, buildPath) => {
+      const path = require('path')
+      const fs = require('fs');
+
+      const outputPath = buildPath.outputPaths.toString();
+      const platform = buildPath.platform.toString();
+
+      if (platform === 'win32') {
+        fs.rmSync(path.join(outputPath, '/resources/app/php/linux/'), { recursive: true, force: true });
+      } else if (platform === 'linux') {
+        fs.rmSync(path.join(outputPath, '/resources/app/php/win32/'), { recursive: true, force: true });
+      }
+    }
+  },
   publishers: []
 }
