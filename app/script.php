7e76b89ae4197783ec7216f034957a2a8759a7d0
mestredainfo <>
2024-06-01 20:41:41 -0300

    exemplo de script

---
+++ app/script.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Script</title>
+</head>
+
+<body>
+    <script>
+        function mensagem() {
+            window.miapp.mensagem('Mensagem MIApp', 'Este é um exemplo de menu com script', 'question');
+        }
+    </script>
+</body>
+
+</html>
