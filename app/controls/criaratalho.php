9e365bc703b07e92d93b6df7c9a7c02dbf059492
mestredainfo <>
2024-06-03 18:59:50 -0300

    Melhorado o código criar atalho

--- app/controls/criaratalho.php
+++ app/controls/criaratalho.php
@@ -9,15 +9,15 @@ if (PHP_OS == 'Linux') {
     $sFolder = '/home/' . get_current_user() . '/.local/share/applications/';
     if (file_exists($sFolder)) {
         $sDesktop = '[Desktop Entry]
-Version=' . file_get_contents($documentroot . '/version') . '
-Name=' . $config['app'] . '
-Comment=' . $config['description'] . '
+Version=' . $aConfig['update']['version'] . '
+Name=' . $aConfig['app']['name'] . '
+Comment=' . $aConfig['app']['description'] . '
 Type=Application
-Exec=' . dirname($documentroot, 4) . '/' . str_replace(' ', '', strtolower($config['app'])) . '
-Icon=' . dirname($documentroot, 2) . '/icon/' . str_replace(' ', '', strtolower($config['app'])) . '.png
+Exec=' . dirname($documentroot, 4) . '/' . str_replace(' ', '', strtolower($aConfig['app']['name'])) . '
+Icon=' . dirname($documentroot, 2) . '/icon/' . str_replace(' ', '', strtolower($aConfig['app']['name'])) . '.png
 Categories=Utility;';
 
-        $sCreateFile = file_put_contents($sFolder . '/' . str_replace(' ', '', strtolower($config['app'])) . '.desktop', $sDesktop);
+        $sCreateFile = file_put_contents($sFolder . '/' . str_replace(' ', '', strtolower($aConfig['app']['name'])) . '.desktop', $sDesktop);
         if ($sCreateFile) {
             echo '<script>window.alert(\'Atalho criado no menu iniciar!\');window.location.assign(\'index.php\');</script>';
         } else {
@@ -27,5 +27,5 @@ Categories=Utility;';
         echo '<script>window.alert(\'Não foi possível criar o atalho no menu iniciar!\');window.location.assign(\'index.php\');</script>';
     }
 } else {
-    echo '<script>window.alert(\'No Windows você pode criar um atalho clicando com o botão direito no executável "' . str_replace(' ', '', strtolower($config['app'])) . '.exe" e clicando em "Criar Atalho"!\');window.location.assign(\'index.php\');</script>';
+    echo '<script>window.alert(\'No Windows você pode criar um atalho clicando com o botão direito no executável "' . str_replace(' ', '', strtolower($aConfig['app']['name'])) . '.exe" e clicando em "Criar Atalho"!\');window.location.assign(\'index.php\');</script>';
 }
