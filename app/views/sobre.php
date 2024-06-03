44b9e4d53393a40d49c60b5ecbf13e02c0733a20
mestredainfo <>
2024-06-03 19:01:39 -0300

    Criado uma página de sobre personalizada

---
+++ app/views/sobre.php
@@ -0,0 +1,25 @@
+<?php
+// Copyright (C) 2004-2024 Murilo Gomes Julio
+// SPDX-License-Identifier: GPL-2.0-only
+
+// Organização: Mestre da Info
+// Site: https://linktr.ee/mestreinfo
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $milang->traduzir('Sobre o MIApp'); ?></title>
+    <link rel="stylesheet" href="/plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+    <?php echo aboutApp('', true); ?>
+    <script src="/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/js/script.js"></script>
+</body>
+
+</html>
