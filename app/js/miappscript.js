a19d05cd4a85e73dddc00eafb85d963b244fc028
mestredainfo <murilogomes5792@gmail.com>
2024-06-17 09:37:21 -0300

    Add copyright and license

--- app/js/miappscript.js
+++ app/js/miappscript.js
@@ -1,3 +1,9 @@
+// Copyright (C) 2004-2024 Murilo Gomes Julio
+// SPDX-License-Identifier: GPL-2.0-only
+
+// Organização: Mestre da Info
+// Site: https://linktr.ee/mestreinfo
+
 let miappmenucheck = true;
 try {
     const miappmenu = document.getElementById('miappmenu');
