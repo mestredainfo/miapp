58d62637c4285cf3fdfe77ca5fc204a00771c173
mestredainfo <murilogomes5792@gmail.com>
2024-06-19 08:32:10 -0300

    Add example args

---
+++ app/args.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arguments</title>
+</head>
+
+<body>
+    <p>To get the arguments run the terminal or cmd, go to your folder and type:</p>
+    <ul>
+        <li>Linux: ./miapp test1 test2 test3</li>
+        <li>Windows: miapp.exe test1 test2 test3</li>
+    </ul>
+    <?php
+    if (empty($_ENV['MIAPP_ARGV'])) {
+        echo 'No argument has been found!';
+    } else {
+        echo $_ENV['MIAPP_ARGV'];
+    }
+    ?>
+</body>
+
+</html>
