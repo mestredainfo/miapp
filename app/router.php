5ea22ae49c832c99e42d83f479b10822b61c95ee
mestredainfo <murilogomes5792@gmail.com>
2024-06-19 09:43:33 -0300

    Add license and copyright

--- app/router.php
+++ app/router.php
@@ -1,4 +1,10 @@
 <?php
+// Copyright (C) 2004-2024 Murilo Gomes Julio
+// SPDX-License-Identifier: MIT
+
+// Mestre da Info
+// Site: https://linktr.ee/mestreinfo
+
 if ($_SERVER['REQUEST_URI'] == '/') {
     include_once(dirname(__FILE__) . '/index.php');
 } else {
